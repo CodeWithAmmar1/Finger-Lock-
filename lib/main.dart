6665957f7487e732a72f9979c9bfc4c90112e42d
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-03-25 03:21:10 +0500

    instruction for app

--- lib/main.dart
+++ lib/main.dart
@@ -22,3 +22,30 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// // bulig.gradle
+// minSdk = 23
+
+
+
+
+// // MainActivity.kt       
+// package com.example.app
+
+// import io.flutter.embedding.android.FlutterFragmentActivity
+
+// class MainActivity : FlutterFragmentActivity()
+
+
+
+
+// // AndroidManifest.xml
+//     <!-- Permissions for fingerprint/biometric authentication -->
+//     <uses-permission android:name="android.permission.USE_BIOMETRIC"/>
+//     <uses-permission android:name="android.permission.USE_FINGERPRINT"/>
+//     <uses-feature android:name="android.hardware.fingerprint" android:required="true"/>
+
+
+
+//Dependency
+//  local_auth: ^2.3.0
